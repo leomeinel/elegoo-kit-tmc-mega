0df04767a7ace8118f1d8da579880259f3e387ec
Leopold Johannes Meinel <leo@meinel.dev>
2025-08-28 14:55:20 +0200

    04-rgb-led: Add logic

--- 04-rgb-led/src/main.cpp
+++ 04-rgb-led/src/main.cpp
@@ -10,17 +10,48 @@
 
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RGB_LED_RED 4
+#define RGB_LED_GREEN 5
+#define RGB_LED_BLUE 6
+
+int setRGB(int, int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  const int PINS[3] = {RGB_LED_RED, RGB_LED_GREEN, RGB_LED_BLUE};
+  for (int pin : PINS) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  const int HTML_COLORS[16][3] = {
+      {255, 255, 255}, // HTML_WHITE
+      {192, 192, 192}, // HTML_SILVER
+      {128, 128, 128}, // HTML_GRAY
+      {0, 0, 0},       // HTML_BLACK
+      {255, 0, 0},     // HTML_RED
+      {128, 0, 0},     // HTML_MAROON
+      {255, 255, 0},   // HTML_YELLOW
+      {128, 128, 0},   // HTML_OLIVE
+      {0, 255, 0},     // HTML_LIME
+      {0, 128, 0},     // HTML_GREEN
+      {0, 255, 255},   // HTML_AQUA
+      {0, 128, 128},   // HTML_TEAL
+      {0, 0, 255},     // HTML_BLUE
+      {0, 0, 128},     // HTML_NAVY
+      {255, 0, 255},   // HTML_FUCHSIA
+      {128, 0, 128}    // HTML_PURPLE
+  };
+  for (const int(&color)[3] : HTML_COLORS) {
+    setRGB(color[0], color[1], color[2]);
+    delay(1000);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) { return x + y; }
+int setRGB(int red, int green, int blue) {
+  analogWrite(RGB_LED_RED, red);
+  analogWrite(RGB_LED_GREEN, green);
+  analogWrite(RGB_LED_BLUE, blue);
+  return 0;
+}
