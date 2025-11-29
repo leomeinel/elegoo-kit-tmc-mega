64707f9618344e6e203ecf47d2bbcbd40ececf90
Leopold Johannes Meinel <leo@meinel.dev>
2025-11-29 17:41:27 +0100

    pio: Move compile_commands.json to build/

--- 25-the-serial-monitor/init.sh
+++ 25-the-serial-monitor/init.sh
@@ -11,8 +11,15 @@
 # Fail on error
 set -e
 
+# Set ${SCRIPT_DIR}
+SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${0}")")"
+
+# Out of source build
+mkdir "${SCRIPT_DIR}"/build || true
+
 # Install required packages
-pio pkg install
+pio pkg install -d "${SCRIPT_DIR}"
 
 # Generate compile_commands.json
-pio run -t compiledb
+pio run -t compiledb -d "${SCRIPT_DIR}"
+mv "${SCRIPT_DIR}"/compile_commands.json "${SCRIPT_DIR}"/build/
