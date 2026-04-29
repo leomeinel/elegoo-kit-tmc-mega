2df44c6cb24d1caebffd29bab2daf2e2c311e809
Leopold Johannes Meinel <leo@meinel.dev>
2026-04-29 23:39:42 +0200

    comments: Remove copyright headers
    I no longer want to use copyright headers. I had to modify them in most
    of my projects multiple times over the last years.

--- 05-digital-inputs/src/main.cpp
+++ 05-digital-inputs/src/main.cpp
@@ -1,12 +1,3 @@
-/*
- * File: main.cpp
- * Author: Leopold Johannes Meinel (leo@meinel.dev)
- * -----
- * Copyright (c) 2025 Leopold Johannes Meinel & contributors
- * SPDX ID: Apache-2.0
- * URL: https://www.apache.org/licenses/LICENSE-2.0
- */
-
 #include <Arduino.h>
 
 constexpr uint8_t PIN_SW1 = 5;
